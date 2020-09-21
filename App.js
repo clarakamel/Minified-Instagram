47332d0bdd3703ee06a1227a180cd13fcb4ea5a0
clarakamel <clara_atef@hotmail.com>
2020-09-21 14:00:45 +0200

    added navigation

--- App.js
+++ App.js
@@ -1,11 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React, {useState, useEffect} from 'react';
 import {StyleSheet, TouchableWithoutFeedback, Keyboard} from 'react-native';
 
@@ -15,10 +7,18 @@ import {
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
-import {View, Text} from 'react-native';
 import auth from '@react-native-firebase/auth';
-import Login from './src/login';
-import Header from './src/header';
+import Login from './src/screens/login';
+import NewsFeed from './src/screens/newsfeed';
+import Profile from './src/screens/profile';
+import BucketList from './src/screens/bucketlist';
+import Navigation from './src/components/navigation';
+import AddPost from './src/screens/addpost';
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import AppNavigator from './src/routes/AppNavigator';
 
 const App: () => React$Node = () => {
   // Set an initializing state whilst Firebase connects
@@ -36,25 +36,50 @@ const App: () => React$Node = () => {
     return subscriber; // unsubscribe on unmount
   });
 
-//sign out method
+  //sign out method
   // auth()
   //   .signOut()
   //   .then(() => console.log('User signed out!'));
+
+  const Stack = createStackNavigator();
 
   if (initializing) return null;
   if (!user) {
     return (
       <>
-        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-          <Login />
-        </TouchableWithoutFeedback>
+        <NavigationContainer>
+          <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+            <Stack.Navigator initialRouteName="NewsFeed">
+              <Stack.Screen name="Login" component={Login} />
+            </Stack.Navigator>
+          </TouchableWithoutFeedback>
+        </NavigationContainer>
+        {/* <AppNavigator /> */}
       </>
     );
   }
+  const Tab = createBottomTabNavigator();
+
   return (
-    <View>
-      <Header />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Newsfeed"
+          component={NewsFeed}
+          options={{title: 'NewsFeed'}}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={Profile}
+          options={{title: 'Profile'}}
+        />
+        <Tab.Screen
+          name="BucketList"
+          component={BucketList}
+          options={{title: 'Bucketlist'}}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 };
 
