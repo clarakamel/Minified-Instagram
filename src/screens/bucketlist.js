a3f43ce60fb95571d0095887cd246a5a0cb7fe73
clarakamel <clara_atef@hotmail.com>
2020-09-26 14:18:36 +0200

    successfully input and add a place to visit

--- src/screens/bucketlist.js
+++ src/screens/bucketlist.js
@@ -1,9 +1,98 @@
 /* eslint-disable prettier/prettier */
-import * as React from 'react';
-import {Button, View, Text, StyleSheet} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import React, {useState, useEffect} from 'react';
+import {
+  Button,
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+  FlatList,
+} from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import BucketlistItem from './bucketlistItem';
+// import {Ionicons} from '@expo/vector-icons';
 
-export default function BucketList() {
-  return <Text>Bucket List</Text>;
-}
+const BucketList = () => {
+  const [place, setPlace] = useState([
+    {value: 'Cairo', key: '1'},
+    {value: 'Spain', key: '2'},
+    {value: 'Mexico', key: '3'},
+  ]);
+  const [text, setText] = useState('');
+
+  //storing the text written in text input
+  const inputHandler = (val) => {
+    setText(val);
+  };
+
+  //storing the bucketlist items
+  const storeData = async (value) => {
+    try {
+      await AsyncStorage.setItem('ChosenPlace', JSON.stringify(place));
+      setPlace((prevPlaces) => {
+        return [{value: text, key: Math.random.toString()}, ...prevPlaces];
+      });
+      console.log('in storedata', place);
+    } catch (e) {
+      Alert.alert(e);
+    }
+  };
+
+  return (
+    <View>
+      <TextInput
+        style={styles.inputField}
+        placeholder="Place"
+        onChangeText={inputHandler}
+      />
+
+      <TouchableOpacity onPress={() => storeData()}>
+        <Text>Add</Text>
+      </TouchableOpacity>
+
+      <View style={styles.list}>
+        <FlatList
+          data={place}
+          renderItem={({item}) => (
+            <Text style={styles.itemText}>{item.value}</Text>
+          )}
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    marginTop: 30,
+  },
+  addButton: {
+    width: 100,
+    // marginTop: 200,
+    alignSelf: 'center',
+  },
+  inputField: {
+    marginBottom: 10,
+    paddingHorizontal: 8,
+    paddingVertical: 6,
+    borderBottomWidth: 1,
+    width: 250,
+    alignSelf: 'center',
+  },
+  list: {
+    marginTop: 50,
+    alignItems: 'center',
+  },
+  itemText: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+});
+
+export default BucketList;
