443aeee6b5237139f1f0f35507f27c9e2b162388
clarakamel <clara_atef@hotmail.com>
2020-09-26 13:10:25 +0200

    provided App.js with redux store

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
+/* eslint-disable prettier/prettier */
 /**
  * @format
  */
-
+import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {createStore} from 'redux';
+import appReducer from './src/redux/reducers/appReducers';
+import {Provider} from 'react-redux';
+import configureStore from './src/redux/configureStore';
+import {loginUser} from './src/redux/actions/appActions';
+import appActions from './src/redux/actions/appActions';
 
-AppRegistry.registerComponent(appName, () => App);
+const store = configureStore();
+
+const RNRedux = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+AppRegistry.registerComponent(appName, () => RNRedux);
