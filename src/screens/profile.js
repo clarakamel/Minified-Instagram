cbd57b3c4fba5bb5665b8ba377b2c8df2cef04a2
clarakamel <clara_atef@hotmail.com>
2020-09-27 08:59:02 +0200

    added header/rendering name and email/added styles

--- src/screens/profile.js
+++ src/screens/profile.js
@@ -1,11 +1,53 @@
 /* eslint-disable prettier/prettier */
 import * as React from 'react';
-import {Button, View, Text, StyleSheet} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import {View, Text, StyleSheet} from 'react-native';
+import auth from '@react-native-firebase/auth';
+import firebase from '@react-native-firebase/app';
+import {Header} from 'react-native-elements';
 
-export default function Profile({navigation}) {
-    return (
-        <Text>Profile</Text>
-    );
+export default function Profile() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.headerContainer}>
+        <Header
+          centerComponent={{
+            text: 'Profile',
+            style: {color: '#fff'},
+          }}
+          centerContainerStyle={{flex: 3}}
+        />
+      </View>
+      <Text style={styles.nameContainer}>
+        Name: {firebase.auth().currentUser.displayName}
+      </Text>
+      <Text style={styles.emailContainer}>
+        E-mail: {firebase.auth().currentUser.email}
+      </Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  nameContainer: {
+    color: 'blue',
+    fontSize: 20,
+    alignItems: 'center',
+    position: 'absolute',
+  },
+  emailContainer: {
+    color: 'blue',
+    fontSize: 20,
+    alignSelf: 'center',
+    marginBottom: 300,
+  },
+  headerContainer: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    width: 500,
+  },
+});
