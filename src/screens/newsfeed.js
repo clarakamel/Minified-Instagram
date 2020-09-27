8467f1aa5278e88c3eb297b1c73220173ee3d26d
clarakamel <clara_atef@hotmail.com>
2020-09-27 08:58:04 +0200

    added header/styles

--- src/screens/newsfeed.js
+++ src/screens/newsfeed.js
@@ -1,22 +1,34 @@
 /* eslint-disable prettier/prettier */
-import * as React from 'react';
-import {Button, View, Text, StyleSheet} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
+import React from 'react';
+import {Button, SafeAreaView, StyleSheet, View} from 'react-native';
+import auth from '@react-native-firebase/auth';
 import {createStackNavigator} from '@react-navigation/stack';
-import Navigator from '../components/navigation';
-import axios from 'react';
-
-
+import {Header} from 'react-native-elements';
 
 export default function NewsFeed({navigation}) {
+  const signOut = () => {
+    auth()
+      .signOut()
+      .then(() => console.log('User signed out!'));
+  };
+
+  const Stack = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Hello There!</Text>
-      <Button
-        title="Add a Post"
-        onPress={() => navigation.navigate('AddPost')}
-      />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.headerContainer}>
+        <Header
+          centerComponent={{
+            text: 'Newsfeed',
+            style: {color: '#fff'},
+          }}
+          centerContainerStyle={{flex: 3}}
+        />
+      </View>
+      <View style={styles.buttonsContainer}>
+        <Button title="Logout" color="#841584" onPress={() => signOut()} />
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -25,5 +37,13 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  headerContainer: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    width: 500,
+  },
+  buttonsContainer: {
+    position: 'absolute',
   },
 });
