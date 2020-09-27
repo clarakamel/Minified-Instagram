93a6a1ee96c10cd0ba601cb386b4aef213192633
clarakamel <clara_atef@hotmail.com>
2020-09-27 08:51:55 +0200

    removed unnecessary imports

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
-/* eslint-disable prettier/prettier */
-/**
- * @format
- */
 import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import {createStore} from 'redux';
-import appReducer from './src/redux/reducers/appReducers';
 import {Provider} from 'react-redux';
 import configureStore from './src/redux/configureStore';
-import {loginUser} from './src/redux/actions/appActions';
-import appActions from './src/redux/actions/appActions';
 
 const store = configureStore();
 
