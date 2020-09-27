d5a01303a41c3c2efee51aba45dd94031bf94e62
clarakamel <clara_atef@hotmail.com>
2020-09-27 08:55:45 +0200

    removed unnecessary imports/added header/added styles

--- src/screens/bucketlist.js
+++ src/screens/bucketlist.js
@@ -1,81 +1,100 @@
 /* eslint-disable prettier/prettier */
 import React, {useState, useEffect} from 'react';
 import {
-  Button,
   View,
   Text,
   StyleSheet,
-  SafeAreaView,
   TextInput,
   TouchableOpacity,
   Alert,
   FlatList,
+  TouchableWithoutFeedback,
+  Keyboard,
 } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
-import BucketlistItem from './bucketlistItem';
-// import {Ionicons} from '@expo/vector-icons';
+import {Header} from 'react-native-elements';
 
 const BucketList = () => {
-  const [place, setPlace] = useState([
-    {value: 'Cairo', key: '1'},
-    {value: 'Spain', key: '2'},
-    {value: 'Mexico', key: '3'},
-  ]);
+  const [place, setPlace] = useState([]);
   const [text, setText] = useState('');
 
   //storing the text written in text input
-  const inputHandler = (val) => {
+  const inputHandler = async (val) => {
+    await AsyncStorage.setItem('ChosenPlace', JSON.stringify(place));
     setText(val);
   };
 
   //storing the bucketlist items
-  const storeData = async (value) => {
+  const storeData = async (place) => {
     try {
-      await AsyncStorage.setItem('ChosenPlace', JSON.stringify(place));
-      setPlace((prevPlaces) => {
+      await setPlace((prevPlaces) => {
         return [{value: text, key: Math.random.toString()}, ...prevPlaces];
       });
-      console.log('in storedata', place);
+      await AsyncStorage.setItem('ChosenPlace', JSON.stringify(place));
+      console.log('in storedata', JSON.stringify(place));
     } catch (e) {
       Alert.alert(e);
     }
   };
 
   return (
-    <View>
-      <TextInput
-        style={styles.inputField}
-        placeholder="Place"
-        onChangeText={inputHandler}
-      />
-
-      <TouchableOpacity onPress={() => storeData()}>
-        <Text>Add</Text>
-      </TouchableOpacity>
-
-      <View style={styles.list}>
-        <FlatList
-          data={place}
-          renderItem={({item}) => (
-            <Text style={styles.itemText}>{item.value}</Text>
-          )}
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.container}>
+        <View style={styles.headerContainer}>
+          <Header
+            centerComponent={{
+              text: 'Bucketlist',
+              style: {color: '#fff'},
+            }}
+            centerContainerStyle={{flex: 3}}
+          />
+        </View>
+        <TextInput
+          style={styles.inputField}
+          placeholder="Place"
+          onChangeText={inputHandler}
         />
+
+        <TouchableOpacity
+          onPress={() => storeData(place)}
+          style={styles.addButton}>
+          <Text>Add Place</Text>
+        </TouchableOpacity>
+
+        <View style={styles.list}>
+          <FlatList
+            data={place}
+            renderItem={({item}) => (
+              <Text style={styles.itemText}>{item.value}</Text>
+            )}
+          />
+        </View>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row',
     justifyContent: 'flex-start',
     marginTop: 30,
   },
   addButton: {
-    width: 100,
-    // marginTop: 200,
+    borderRadius: 5,
+    width: 150,
+    height: 50,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+    marginLeft: 125,
+  },
+  headerContainer: {
+    width: 500,
+    height: 200,
     alignSelf: 'center',
+    marginTop: -30,
   },
   inputField: {
     marginBottom: 10,
